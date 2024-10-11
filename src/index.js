dc96752c7fb167bf3081e0f57119e8355b173b27
Jonathan Young <jy>
2024-10-11 13:46:56 -0700

    axios not authenticating for htis API. use fetch.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 const cityInput = document.getElementById('cityInput');
-const SHOULD_BE_IN_ENV="JJPPTP2Z7DMHG2KAFAX5B7745S"
+const SHOULD_BE_IN_ENV='JPPTP2Z7DMHG2KAFAX5B7745S'
 let weatherData = '';
-
 
 const getWeatherData = async (cityName) => {
     try {
         const validCityName = encodeURIComponent(cityName); 
         //encodeURIComponent allows a string to be inserted into a URI template literal
-        const response = await axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${validCityName}?key=${SHOULD_BE_IN_ENV}`), 
-
-
+        const data = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${validCityName}?key=${SHOULD_BE_IN_ENV}`);
         //loading
-        weatherData = response.data
+        weatherData = await data.json();
         showWeather();
 
 		console.log(weatherData.currentConditions); 
